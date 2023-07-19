877ec765ff1b0e537140524c063e75ab6c20d126
alessiofranchini <123971820+alessiofranchini@users.noreply.github.com>
2023-07-19 13:32:34 +0200

    ok

--- capstoneAuto-main/src/main/java/com/alessio/repository/AutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alessio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alessio.entity.Auto;
-
-public interface AutoRepository extends JpaRepository<Auto, Long>{
-    
-}
