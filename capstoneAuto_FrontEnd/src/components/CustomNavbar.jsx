c58b4eebe587e2bbefb6dbdd0641ccf24f834243
alessiofranchini <123971820+alessiofranchini@users.noreply.github.com>
2023-07-19 23:59:47 +0200

    aggiunta logo

--- capstoneAuto_FrontEnd/src/components/CustomNavbar.jsx
+++ capstoneAuto_FrontEnd/src/components/CustomNavbar.jsx
@@ -1,19 +1,34 @@
-import { Navbar, Nav, } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 
 function CustomNavbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div>
-      <Navbar expand="lg" className="custom-navbar">
+      <Navbar expand="lg" className="custom-navbar p-0">
         <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"  className="navbar-button border-0" style={{ outline: 'none', boxShadow: 'none' }}/>
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            className={`navbar-button border-0 ${menuOpen ? 'collapsed' : ''}`}
+            style={{ outline: 'none', boxShadow: 'none' }}
+            onClick={handleToggleMenu}
+          />
+          <Navbar.Collapse id="basic-navbar-nav" className={menuOpen ? 'show' : ''}>
             <Nav className="me-auto">
               <Nav.Link href="/home" className='nav-text'>Home</Nav.Link>
               <Nav.Link href="/collezioni" className='nav-text'>Collezioni</Nav.Link>
               <Nav.Link href="/cliente" className='nav-text'>Cliente</Nav.Link>
             </Nav>
           </Navbar.Collapse>
+          <Navbar.Brand href="/home" className={`ms-auto d-none d-lg-block ${menuOpen ? 'd-lg-none' : ''}`}>
+            <img className='logo' src="/assets/img/Black n White Luxury  Rent Car Logo.png" />
+          </Navbar.Brand>
         </Container>
       </Navbar>
     </div>
